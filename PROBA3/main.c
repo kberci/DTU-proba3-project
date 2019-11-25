8451931fa9db454e8887b1b54421cdbadbd1ed7c
Bertalan Kovács <k.berci95@gmail.com>
2019-11-25 11:02:07 +0100

    folytasd

--- PROBA3/main.c
+++ PROBA3/main.c
@@ -1,7 +1,7 @@
 ﻿#include "main.h"
 
 
-int main(int argc, char* argv[]) {
+/*int main(int argc, char* argv[]) {
 	CvMat* S = cvCreateMat(4, 4, CV_32FC1); // matrix that stores the distances of the LEDs from each other, constant
 	GetDistancesMatrix(S);
 	
@@ -53,6 +53,130 @@ int main(int argc, char* argv[]) {
 	CalculateAffineTransform(points_L, points_R, &T);
 	CvPoint2D32f pl;
 	ApplyAffineTransform(cvPoint2D32f(909,979), T, &pl);
+
+
+	// -------------------------------------- BERCI VISUALIZATION--------------------------------------------//
+	// output image conversion to 3 channels (so that contour can be coloured)
+	IplImage* output = cvCreateImage(cvSize(image->width, image->height), IPL_DEPTH_8U, 3);
+	cvConvertImage(image, output, CV_GRAY2BGR);
+
+	for (int point_num = 0; point_num < TOTAL_POINTS; point_num++) {
+		// draw contour
+		for (int i = 1; i < point_set[point_num].used; i++) {
+			// draw contour line to output image
+			CvPoint p1 = cvPoint(point_set[point_num].array[i - 1].x, point_set[point_num].array[i - 1].y);
+			CvPoint p2 = cvPoint(point_set[point_num].array[i].x, point_set[point_num].array[i].y);
+			cvLine(output, p1, p2, cvScalar(0, 0, 255, 0), 1, 8, 0);
+		}
+		// draw center
+		for (int i = 1; i < point_set[point_num].used; i++) {
+			// draw center to output image
+			int cross_size = 5;
+			cvLine(output, cvPoint(centers[point_num].x - cross_size, centers[point_num].y - cross_size),
+				cvPoint(centers[point_num].x + cross_size, centers[point_num].y + cross_size), cvScalar(0, 255, 0, 0), 2, 0, 0);
+			cvLine(output, cvPoint(centers[point_num].x - cross_size, centers[point_num].y + cross_size),
+				cvPoint(centers[point_num].x + cross_size, centers[point_num].y - cross_size), cvScalar(0, 255, 0, 0), 2, 0, 0);
+		}
+	}
+	int cross_size = 2;
+	for (int point_num = 0; point_num < 4; point_num++) {
+		// cross on selected points:
+		cvLine(output, cvPoint(points3678[point_num].x - cross_size, points3678[point_num].y - cross_size),
+			cvPoint(points3678[point_num].x + cross_size, points3678[point_num].y + cross_size), cvScalar(255, 0, 0, 0), 2, 0, 0);
+		cvLine(output, cvPoint(points3678[point_num].x - cross_size, points3678[point_num].y + cross_size),
+			cvPoint(points3678[point_num].x + cross_size, points3678[point_num].y - cross_size), cvScalar(255, 0, 0, 0), 2, 0, 0);
+		cross_size += 3;
+	}
+
+	// show images
+	cvShowImage("Original image", image);
+	cvShowImage("Contoured image", output);
+
+	cvWaitKey(0);
+	cvReleaseImage(&image);
+	cvDestroyAllWindows();
+	for (int point_num = 0; point_num < TOTAL_POINTS; point_num++) {
+		FreeArray(&point_set[point_num]);
+	}
+	// -------------------------------------- BERCI VISUALIZATION --------------------------------------------//
+
+	return 0;
+}*/
+
+int main(int argc, char* argv[]) {
+	CvMat* S = cvCreateMat(4, 4, CV_32FC1); // matrix that stores the distances of the LEDs from each other, constant
+	GetDistancesMatrix(S);
+
+	CvCapture* capture = cvCaptureFromCAM(0); // initialize capture device
+
+	while (1) {
+		if (!cvGrabFrame(capture)) { // capture a frame
+			printf("Could not grab a frame\n\7");
+			break;
+		}
+
+		IplImage* image_binary = cvRetrieveFrame(capture);
+
+		// show images
+		cvResizeWindow(wName, gray->width, gray->height);
+		cvShowImage(wName, gray);
+
+		int key = cvWaitKey(1000 / 30);
+		if (key == 27) {
+			cvReleaseImage(&gray);
+			break;
+		}
+		switch (key) {
+		case 's':
+			cvSaveImage("gray.png", gray);
+			break;
+		default:
+			break;
+		}
+	}
+	IplImage* image_binary = cvCloneImage(image);
+
+	ERROR ecode = CreateBinary(image, image_binary, 180);
+	if (ERREVAL(ecode)) {
+		EPRINT(ecode);
+		return -1;
+	}
+
+	CvPoint start = cvPoint(0, 0);
+	Array point_set[TOTAL_POINTS];
+	CvPoint2D32f centers[TOTAL_POINTS];
+
+	FindAllCenters(image_binary, start, point_set, centers);
+
+	CvPoint2D32f points3678[4];
+	ChoosePoints3678(centers, points3678);
+
+	CvPoint2D32f C[4]; // target points (x,y) in camera coordinates
+	PixelToCameraCoordinate(points3678, C);
+
+	CvPoint3D32f points[4]; // returned 4 points (x,y,z) are stored in this array
+	GetP4PAbidi(S, C, points);
+
+
+
+
+	// cvPoint(col,row) <- this is native to OpenCv
+	CvPoint points_L[3] = {
+		cvPoint(1280,611),
+		cvPoint(1277,979),
+		cvPoint(974,673)
+	};
+
+	CvPoint points_R[3] = {
+		cvPoint(1208,648),
+		cvPoint(1181,1016),
+		cvPoint(900,690)
+	};
+
+	Affine T;
+	CalculateAffineTransform(points_L, points_R, &T);
+	CvPoint2D32f pl;
+	ApplyAffineTransform(cvPoint2D32f(909, 979), T, &pl);
 
 
 	// -------------------------------------- BERCI VISUALIZATION--------------------------------------------//
